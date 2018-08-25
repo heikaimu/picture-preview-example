37374972e56291baab2d0a4e0e99444509ad5093
heikaimu <576079353@qq.com>
2018-08-25 19:34:03 +0800

    文字修改

--- src/view/other.vue
+++ src/view/other.vue
@@ -3,6 +3,7 @@
       <div>
         <a class="address" href="https://github.com/heikaimu/vue-picture-preview">github地址</a>
         <p>插件以后功能会逐渐增加</p>
+        <p>如果有问题，可以给我卸载issues里</p>
         <p>如果喜欢该插件可以给我赏一个star哦(#^.^#)</p>
       </div>
     </div>
